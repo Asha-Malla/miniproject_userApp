7e92938926ed7d94401d1de6c8a9e12af153f2a6
Malla Asha <63631167+Asha-Malla@users.noreply.github.com>
2021-07-07 09:41:23 +0530

    Update splash_screen.dart

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -2,11 +2,8 @@ import 'dart:async';
 
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-//import 'package:fluttertoast/fluttertoast.dart';
 import 'package:hospital_management/homepage.dart';
 import 'package:hospital_management/screens/login-screen.dart';
-//import 'package:sample_firebase_project_authentication/screens/home_screen.dart';
-//import 'package:sample_firebase_project_authentication/screens/login_screen.dart';
 
 class SplashScreen extends StatefulWidget {
   //const SplashScreen({ Key? key }) : super(key: key);
@@ -20,18 +17,18 @@ class _SplashScreenState extends State<SplashScreen> {
   void initState() {
     super.initState();
     Timer(Duration(seconds: 5), () {           //to show 5 sec of splash screen
-      FirebaseAuth.instance.authStateChanges().listen((value) {
+      FirebaseAuth.instance.authStateChanges().listen((value) {   //check user authentication 
         if (value == null) {
           //not logged in
           Navigator.pushAndRemoveUntil(
               context,
-              MaterialPageRoute(builder: (_) => LoginScreen()),
+              MaterialPageRoute(builder: (_) => LoginScreen()),  //redirect to login screen if not logged in
               (Route<dynamic> rr) => false);
         } else {
           //logged in
           Navigator.pushAndRemoveUntil(
               context,
-              MaterialPageRoute(builder: (_) => Homepage()),
+              MaterialPageRoute(builder: (_) => Homepage()),    //redirect to homepage screen if already logged in
               (Route<dynamic> rr) => false);
         }
       });
@@ -44,8 +41,8 @@ class _SplashScreenState extends State<SplashScreen> {
         height: double.infinity,
         width: double.infinity,
         child: Image.asset(
-          "assets/splash_icon2.png",
-          fit: BoxFit.fill,
+          "assets/splash_icon2.png",   //splash screen icon  //displays for 5 secs
+          fit: BoxFit.fill,            //fits entire screen
         ),
       ),
     );
